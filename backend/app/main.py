a721c197536a55b13c329bc93804f8190da7d613
liritan <dianam04@mail.ru>
2026-02-24 20:28:24 +0400

    CORS Configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://diplom-3gfa.vercel.app",  # Vercel domain
 ]
 
 app.add_middleware(
